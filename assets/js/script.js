e11172a5c0046a2d579fdf87d8d31182e284327e
Maadajibao <>
2024-02-08 19:08:44 +0000

    Create playGame functionand event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,32 @@ const computerImage = document.getElementById('computer-image');
 const messages = document.getElementById('messages');
 const choices = ['rock','paper', 'scissors']
 
+/**
+ * Add event listener to all the buttons
+ */
 
+for (let button of buttons) {
+    let playerChoice = this.getAttribute('data-choice');
+    playGame(playerChoice)
+}
+
+/**
+ * The main game function. Accets one parameter, which 
+ * is the data-choice value of the selected button
+ */
+
+
+playGame (playerChoice) {
+
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
+
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+    computerImage.alt = choices[computerChoice];
+
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+    updateScores(result)
+}
